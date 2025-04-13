9b97e30f9a55d70eb5e2a1ddcc419d75680b2a60
guywal <guywaldman1@gmail.com>
2025-04-13 21:34:08 +0100

    add install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e #exit on error
+
+# exist checks if a command exist in shell
+exist() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# install homebrew
+# https://brew.sh/
+if exist brew; then
+    log "Updating Homebrew..."
+    brew update
+else
+  (echo '🍺  Installing Homebrew'
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+fi
+
+# install chezmoi
+# https://www.chezmoi.io/
+if ! exist chezmoi; then
+    echo '👊  Installing chezmoi'
+    brew install chezmoi
+
+chezmoi init https://github.com/guywal/dotfiles.git
+
+echo "Run 'chezmoi diff' to see what changes would apply, then run 'chezmoi apply'"
